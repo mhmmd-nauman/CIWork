f5d9af42938459c072b1c7a15d81bd772ebb7a53
Muhammad Nauman <mhmmd.nauman@gmail.com>
2015-02-25 13:49:11 -0800

    fixed bootstrap and jquery links

--- application/views/admin/components/page_head.php
+++ application/views/admin/components/page_head.php
@@ -4,21 +4,21 @@
 	<meta charset="UTF-8">
 	<title><?php echo $meta_title; ?></title>
 	<!-- Bootstrap -->
-	<link href="<?php echo site_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
-	<link href="<?php echo site_url('css/admin.css'); ?>" rel="stylesheet">
-	<link href="<?php echo site_url('css/datepicker.css'); ?>" rel="stylesheet">
+	<link href="<?php echo site_url('asset/css/bootstrap.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo site_url('asset/css/admin.css'); ?>" rel="stylesheet">
+	<link href="<?php echo site_url('asset/css/datepicker.css'); ?>" rel="stylesheet">
 		
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script src="<?php echo site_url('js/bootstrap.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/bootstrap-datepicker.js'); ?>"></script>
+	<script src="<?php echo site_url('asset/js/bootstrap.min.js'); ?>"></script>
+	<script src="<?php echo site_url('asset/js/bootstrap-datepicker.js'); ?>"></script>
 	<?php if(isset($sortable) && $sortable === TRUE): ?>
-	<script src="<?php echo site_url('js/jquery-ui-1.9.1.custom.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/jquery.mjs.nestedSortable.js'); ?>"></script>
+	<script src="<?php echo site_url('asset/js/jquery-ui-1.9.1.custom.min.js'); ?>"></script>
+	<script src="<?php echo site_url('asset/js/jquery.mjs.nestedSortable.js'); ?>"></script>
 	<?php endif; ?>
 	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<!-- TinyMCE -->
-	<script type="text/javascript" src="<?php echo site_url('js/tiny_mce/tiny_mce.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo site_url('asset/js/tiny_mce/tiny_mce.js'); ?>"></script>
 	<script type="text/javascript">
 		tinyMCE.init({
 			// General options
